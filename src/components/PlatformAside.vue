805fbb408585417cd3eeb9d14624d0519fbfb785
zixianSong <1456505999@qq.com>
2025-06-16 15:07:20 +0800

    hello world为主页面

---
+++ src/components/PlatformAside.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <el-container>
+    <el-aside style="background-color: green;height: 80vh">
+      this is aside
+    </el-aside>
+  </el-container>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PlatformHeader.vue
@@ -0,0 +1,17 @@
+<style scoped>
+
+</style>
+
+
+<template>
+  <el-container>
+    <el-header style="height:60px;background-color: red">
+      this is header
+    </el-header>
+  </el-container>
+
+</template>
+
+<script setup>
+
+</script>
